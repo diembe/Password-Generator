93f1d0e636ab6a661dec562b98e303ff083cf95d
diembe <contact@antibean.com>
2020-06-11 02:10:57 -0600

    Initial generator completed.

--- script.js
+++ script.js
@@ -8,6 +8,218 @@ function writePassword() {
 
   passwordText.value = password;
 
+  function generatePassword() {
+
+    var lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+    var uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numericCharacters = '0123456789';
+    var specialCharacters = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+
+
+    // Function to get the password length
+    function getLength() {
+      var pLength = prompt("How long would you like your password to be? (must be between 8 and 128 characters)");
+      if (pLength < 8 || pLength > 128) {
+        alert("Sorry, your password must be between 8 and 128 characters.");
+        getLength();
+      } else {
+        return pLength;
+      }
+    }
+
+    var passwordLength = getLength();
+    console.log(passwordLength);
+
+    // Yes/No for Lowercase
+    function chooseLowercase() {
+      var lowercaseAnswer = prompt("Would you like to include lowercase letters? (Yes/No)");
+      if (lowercaseAnswer === "Yes" || lowercaseAnswer === "yes") {
+        alert("Roger that, we'll include lower case.");
+        //console.log(lowercaseAnswer);
+        return lowercaseAnswer;
+      } else if (lowercaseAnswer === "No" || lowercaseAnswer === "no") {
+        alert("Roger that, no lower case.");
+        //console.log(lowercaseAnswer);
+        return lowercaseAnswer;
+      } else {
+        alert("Please select either Yes or No");
+        chooseLowercase();
+      }
+    }
+
+    var lowercase = chooseLowercase();
+    lowercase = lowercase.toLowerCase();
+    console.log(lowercase);
+
+    // Yes/No for Uppercase
+    function chooseUppercase() {
+      var uppercaseAnswer = prompt("Would you like to include uppercase letters? (Yes/No)");
+      if (uppercaseAnswer === "Yes" || uppercaseAnswer === "yes") {
+        alert("Roger that, we'll include uppercase.");
+        //console.log(lowercaseAnswer);
+        return uppercaseAnswer;
+      } else if (uppercaseAnswer === "No" || uppercaseAnswer === "no") {
+        alert("Roger that, no uppercase.");
+        //console.log(lowercaseAnswer);
+        return uppercaseAnswer;
+      } else {
+        alert("Please select either Yes or No");
+        chooseUppercase();
+      }
+    }
+
+    var uppercase = chooseUppercase();
+    uppercase = uppercase.toLowerCase();
+    console.log(uppercase);
+
+    // Yes/No for Numeric Characters
+    function chooseNumeric() {
+      var numericAnswer = prompt("Would you like to include numbers? (Yes/No)");
+      if (numericAnswer === "Yes" || numericAnswer === "yes") {
+        alert("Roger that, we'll include numbers.");
+        //console.log(lowercaseAnswer);
+        return numericAnswer;
+      } else if (numericAnswer === "No" || numericAnswer === "no") {
+        alert("Roger that, no numbers.");
+        //console.log(lowercaseAnswer);
+        return numericAnswer;
+      } else {
+        alert("Please select either Yes or No");
+        chooseNumeric();
+      }
+    }
+
+    var numeric = chooseNumeric();
+    numeric = numeric.toLowerCase();
+    console.log(numeric);
+
+
+    // Yes/No for Special Characters
+    function chooseSpecial() {
+      var specialAnswer = prompt("Would you like to include special characters? (Yes/No)");
+      if (specialAnswer === "Yes" || specialAnswer === "yes") {
+        alert("Roger that, we'll include special characters.");
+        //console.log(lowercaseAnswer);
+        return specialAnswer;
+      } else if (specialAnswer === "No" || specialAnswer === "no") {
+        alert("Roger that, no special characters.");
+        //console.log(lowercaseAnswer);
+        return specialAnswer;
+      } else {
+        alert("Please select either Yes or No");
+        chooseSpecial();
+      }
+    }
+
+    var special = chooseSpecial();
+    special = special.toLowerCase();
+    console.log(special);
+
+    if (lowercase === 'no' && uppercase === 'no' && numeric === 'no' && special === 'no') {
+      alert("Please choose at least one character type");
+      generatePassword();
+    }
+
+    var combinedCharSet = '';
+
+    if (lowercase === 'yes') {
+      combinedCharSet = combinedCharSet + lowercaseCharacters;
+    }
+
+    if (uppercase === 'yes') {
+      combinedCharSet = combinedCharSet + uppercaseCharacters;
+    }
+
+    if (numeric === 'yes') {
+      combinedCharSet = combinedCharSet + numericCharacters;
+    }
+
+    if (special === 'yes') {
+      combinedCharSet = combinedCharSet + specialCharacters;
+    }
+
+    console.log(combinedCharSet);
+
+    password = '';
+    
+    password = tryPassword();
+    
+    function tryPassword(){
+      var passwordTest = '';
+      for(i = 0; i <= passwordLength; i++) {
+        charStringLength = combinedCharSet.length;
+        charStart = Math.floor((Math.random() * charStringLength) + 1);
+        charEnd = charStart + 1;
+        randomCharacter = combinedCharSet.substring(charStart, charEnd);
+        passwordTest = passwordTest + randomCharacter;
+      }
+      return passwordTest;
+    }
+
+    if (lowercase === 'yes') {
+      var passwordLowercase = 0;
+      for (i = 1; i < lowercaseCharacters.length; i++) {
+        charStop = i + 1;
+        charCheck = lowercaseCharacters.substring(i, charStop);
+        passwordLowercase += password.indexOf(charCheck);
+      }
+
+      lowercaseNegative = -Math.abs(lowercaseCharacters.length);
+      console.log("lowerCaseNegative: " + lowercaseNegative);
+      console.log("passwordLowerCase: " + passwordLowercase);
+
+
+      if (passwordLowercase === lowercaseNegative) {
+        console.log("No lower case;");
+        tryPassword();
+      }
+    }
+
+    if (uppercase === 'yes') {
+      var passwordUppercase = 0;
+      for (i = 1; i < uppercaseCharacters.length; i++) {
+        charStop = i + 1;
+        charCheck = uppercaseCharacters.substring(i, charStop);
+        passwordUppercase += password.indexOf(charCheck);
+      }
+
+      uppercaseNegative = -Math.abs(uppercaseCharacters.length);
+      console.log("uppercaseNegative: " + uppercaseNegative);
+      console.log("passwordUppercase: " + passwordUppercase);
+
+
+      if (passwordUppercase === uppercaseNegative) {
+        console.log("No lower case;");
+        tryPassword();
+      }
+    }
+
+
+    if (numeric === 'yes') {
+      var passwordNumeric = 0;
+      for (i = 1; i < numericCharacters.length; i++) {
+        charStop = i + 1;
+        charCheck = numericCharacters.substring(i, charStop);
+        passwordNumeric += password.indexOf(charCheck);
+      }
+
+      numericNegative = -Math.abs(numericCharacters.length);
+      console.log("numericNegative: " + numericNegative);
+      console.log("passwordNumeric: " + passwordNumeric);
+
+
+      if (passwordNumeric === numericNegative) {
+        console.log("No numeric characters;");
+        tryPassword();
+      }
+    }
+
+
+
+    return password;
+
+  }
+
 }
 
 // Add event listener to generate button
